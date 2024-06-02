512cb552abbff48c81dcb663312a562c0012295a
zabux <109522388+zabux@users.noreply.github.com>
2024-06-02 18:58:57 +0800

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
       <footer className="p-3 text-center mb-20">
         <p>
           Dibuat dengan ❤ oleh{' '}
-          <a href="https://feriirawan.vercel.app" target="_blank">
-            Feri Irawan
+          <a href="https://miaw.my.id" target="_blank">
+            Awy
           </a>{' '}
           {/* untuk Sweet heartnya */}
         </p>
