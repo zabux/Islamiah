0a0d7ae9f67791874d2796226f0e600fbf23e459
zabux <109522388+zabux@users.noreply.github.com>
2024-07-02 09:28:27 +0800

    update components/quotes/QuotesCard.jsx

--- components/quotes/QuotesCard.jsx
+++ components/quotes/QuotesCard.jsx
@@ -1,12 +1,12 @@
-export default function QuotesCard({ quote }) {
+export default function QiblaCard({ qiblaDirection }) {
   return (
     <div className="shadow rounded-xl overflow-hidden hover:scale-105 duration-300">
       <div className="p-3">
         <h2 className="font-bold text-lg md:text-xl text-rose-400 mb-3">
-          "{quote.text}"
+          Arah Kiblat
         </h2>
         <p className="text-right text-sm text-gray-500">
-          - {quote.source}
+          Arah kiblat dari lokasi Anda adalah {qiblaDirection.toFixed(2)}° dari utara.
         </p>
       </div>
     </div>
