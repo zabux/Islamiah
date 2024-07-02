4a7c5289ba0bbfe9db5fa1a1241c422404175347
zabux <109522388+zabux@users.noreply.github.com>
2024-07-02 08:37:59 +0800

    update components/BottomNavigation.jsx

--- components/BottomNavigation.jsx
+++ components/BottomNavigation.jsx
@@ -44,6 +44,26 @@ const navs = [
     ),
   },
   {
+    name: 'Quotes',
+    url: '/quotes',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 mx-auto"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+        />
+      </svg>
+    ),
+  },
+  {
     name: 'Berita',
     url: '/berita',
     icon: (
