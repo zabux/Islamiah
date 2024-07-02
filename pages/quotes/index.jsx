ce5b26073d38397bf3f8ba5d96370ec21a9981bf
zabux <109522388+zabux@users.noreply.github.com>
2024-07-02 08:42:19 +0800

    update pages/quotes/index.jsx

--- pages/quotes/index.jsx
+++ pages/quotes/index.jsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react'
-import BeritaCard from '../../components/berita/BeritaCard'
+import QuotesCard from '../../components/quotes/QuotesCard'
 import ErrorCard from '../../components/ErrorCards'
 import Layout from '../../components/Layouts'
 import Loading from '../../components/Loading'
 
-export default function Berita() {
-  const [berita, setBerita] = useState(null)
+export default function Quotes() {
+  const [quotes, setQuotes] = useState(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
 
   // Fetch data
   useEffect(() => {
     setLoading(true)
-    fetch('https://api-berita-indonesia.vercel.app/republika/islam/')
+    fetch('https://api-quotable.io/quotes')
       .then((res) => res.json())
-      .then(({ data }) => {
-        setBerita(data.posts)
+      .then(({ results }) => {
+        setQuotes(results)
         setLoading(false)
       })
       .catch(() => {
@@ -25,20 +25,20 @@ export default function Berita() {
   }, [])
 
   return (
-    <Layout name="Berita">
-      <h1 className="text-3xl font-bold text-rose-500 mb-3">Berita Islamic</h1>
+    <Layout name="Quotes">
+      <h1 className="text-3xl font-bold text-rose-500 mb-3">Kumpulan Quotes</h1>
 
-      <p>Berikut ini adalah kumpulan berita-berita islamic.</p>
+      <p>Berikut ini adalah kumpulan quotes inspiratif.</p>
 
-      {loading && <Loading message="Memuat berita..." />}
+      {loading && <Loading message="Memuat quotes..." />}
       {error && (
         <ErrorCard message="Gagal memuat data, silakan periksa koneksi internet Anda lalu refresh halaman ini." />
       )}
 
-      {berita && (
+      {quotes && (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5 mt-4">
-          {berita.map((post, i) => (
-            <BeritaCard berita={post} key={i} />
+          {quotes.map((quote, i) => (
+            <QuotesCard quote={quote} key={i} />
           ))}
         </div>
       )}
